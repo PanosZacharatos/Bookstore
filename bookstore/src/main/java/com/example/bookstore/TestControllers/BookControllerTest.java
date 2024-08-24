eef9ea52fa81220ace54a91859a73496a224afa3
PanosZacharatos <panoszah@gmail.com>
2024-08-24 16:37:59 +0300

    implementation of unit testing

--- bookstore/src/main/java/com/example/bookstore/TestControllers/BookControllerTest.java
+++ bookstore/src/main/java/com/example/bookstore/TestControllers/BookControllerTest.java
@@ -3,7 +3,6 @@ package com.example.bookstore.TestControllers;
 import com.example.bookstore.Controllers.BookController;
 import com.example.bookstore.Models.Book;
 import com.example.bookstore.Services.BookService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,20 +15,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import org.springframework.http.MediaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Optional;
-
-import static org.mockito.Mockito.when;
 
 @WebMvcTest(value = BookController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 public class BookControllerTest {
@@ -175,48 +172,156 @@ public class BookControllerTest {
     // TESTS FOR CREATE A NEW BOOK
 
     // test with expected results
-//    @Test
-//    public void createBook() throws Exception{
-//        //set up mock service
-//        Book book = new Book(null,"Test Book 1", "Author 1", "9780451524935", new Date(), 19.45, 100);
-//        Book savedBook = new Book(1L,"Test Book 1", "Author 1", "9780451524935", new Date(), 19.45, 100);
-//        when(bookService.saveBook(any(Book.class))).thenReturn(savedBook);
-//
-//        //perform get request and verify the response is correct
-//        mockMvc.perform(post("/books"))
-//
-//
-//    }
-
-//    @Test
-//    public void testCreateBook_Successful() throws Exception {
-//        Book book = new Book(null, "New Book", "Author Name", "1234567890123", new Date(), 29.99, 50);
-//        Book savedBook = new Book(1L, "New Book", "Author Name", "1234567890123", new Date(), 29.99, 50);
-//        when(bookService.saveBook(any(Book.class))).thenReturn(savedBook);
-//
-//        mockMvc.perform(post("/books")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(mapper.writeValueAsString(book)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id", is(1)))
-//                .andExpect(jsonPath("$.title", is("New Book")));
-//    }
-
-
     @Test
-    public void testCreateBook_Successful() throws Exception {
-        Book book = new Book(null, "New Book", "Author Name", "1234567890123", new Date(), 29.99, 50);
-        Book savedBook = new Book(1L, "New Book", "Author Name", "1234567890123", new Date(), 29.99, 50);
-        when(bookService.saveBook(any(Book.class))).thenReturn(savedBook);
+    public void testCreateBook() throws Exception{
+        Book book = new Book(null,"Book 1","Author 1","1234567890123",new Date(),20.00,20);
+        when(bookService.saveBook(org.mockito.Mockito.any(Book.class))).thenReturn(book);
 
         mockMvc.perform(post("/books")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(book)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(1))) // Assuming 'id' is an integer in JSON
-                .andExpect(jsonPath("$.title", is("New Book"))); // Correct use of 'is' matcher for string comparison
+                .andExpect(jsonPath("$.title", is("Book 1")));
     }
 
+    // test with validation failure
+    @Test
+    public void testCreateBook_ValidationFail() throws Exception{
+        Book book = new Book(null,"","","1234567890123",new Date(),20.00,20);
+        when(bookService.saveBook(org.mockito.Mockito.any(Book.class))).thenReturn(book);
+
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(book)))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    // test with service exception
+    @Test
+    public void testCreateBook_ServiceException() throws Exception{
+        Book book = new Book(null,"Book 1","Author 1","1234567890123",new Date(),20.00,20);
+        when(bookService.saveBook(org.mockito.Mockito.any(Book.class))).thenThrow(new RuntimeException("Unexpected error"));
+
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(book)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("Invalid book data provided")));
+
+    }
+
+    // test with null data
+    @Test
+    public void testCreateBook_NullData() throws Exception{
+        Book book = new Book(null,"Book 1",null,"1234567890123",new Date(),20.00,20);
+        //when(bookService.saveBook(any(Book.class))).thenThrow(new RuntimeException("Unexpected error"));
+
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(book)))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    //test for security
+//    @Test
+//    public void testCreateBook_Security() throws Exception{
+            //TODO
+//    }
+
+
+    // TESTS FOR UPDATE A BOOK
+
+    // test with expected results
+    @Test
+    public void testUpdateBook() throws Exception{
+        Long id = 1L;
+        Book book = new Book(id,"title","author","1234567890123",new Date(),20.00,20);
+        Book updatedBook = new Book(id,"Updated title","author","1234567890123",new Date(),20.00,20);
+        when(bookService.findBookById(id)).thenReturn(Optional.of(book));
+        when(bookService.saveBook(org.mockito.Mockito.any((Book.class)))).thenReturn(updatedBook);
+
+
+        mockMvc.perform(put("/books/{id}",id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedBook)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Updated title")));
+    }
+
+    // test with validation failure
+    @Test
+    public void testUpdateBook_ValidationFailure() throws Exception{
+        Long id = 1L;
+        Book updatedBook = new Book(id,"","","1234567890123",new Date(),20.00,20);
+
+
+
+        mockMvc.perform(put("/books/{id}",id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedBook)))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    // test with service exception
+    @Test
+    public void testUpdateBook_ServiceException() throws Exception{
+        Long id = 1L;
+        Book updatedBook = new Book(id,"title","author","1234567890123",new Date(),20.00,20);
+        when(bookService.findBookById(id)).thenReturn(Optional.empty()); //simulate book not found
+
+
+        mockMvc.perform(put("/books/{id}",id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedBook)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string((containsString("Book not found"))));
+
+    }
+
+    //test for security
+//    @Test
+//    public void testUpdateBook_Security() throws Exception{
+    //TODO
+//    }
+
+    // TESTS FOR DELETE A BOOK
+
+    // test with expected results
+    @Test
+    public void testDeleteBook() throws Exception{
+        Long id = 1L;
+        Book existingBook = new Book();
+        existingBook.setId(id);
+        when(bookService.findBookById(id)).thenReturn(Optional.of(existingBook));
+        doNothing().when(bookService).deleteBook(id);
+
+        mockMvc.perform(delete("/books/{id}", id))
+                .andExpect(status().isOk());
+    }
+
+    // test with book not found
+    @Test
+    public void testDeleteBook_BookNotFound() throws Exception{
+        Long id = 1L;
+        when(bookService.findBookById(id)).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/books/{id}", id))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string((containsString("Book not found"))));
+    }
+
+    //test for security
+//    @Test
+//    public void testDeleteBook_Security() throws Exception{
+    //TODO
+//    }
 
 
 }
+
+
+
+
