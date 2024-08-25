abd2a28b5d5c60443ca1e86a59bcde1b1b4312be
PanosZacharatos <panoszah@gmail.com>
2024-08-25 15:00:50 +0300

    impelment search by author or title

--- bookstore/src/main/java/com/example/bookstore/Repositories/BookRepository.java
+++ bookstore/src/main/java/com/example/bookstore/Repositories/BookRepository.java
@@ -2,7 +2,10 @@ package com.example.bookstore.Repositories;
 
 import com.example.bookstore.Models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
+    Page<Book> findByTitle(String title, Pageable pageable);
+    Page<Book> findByAuthor(String author, Pageable pageable);
 }
